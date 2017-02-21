5c8c355d4e961994caf1a164c1371c0d4a58e9c5
tjvalcourt <tvalcourt7@yahoo.com>
2017-02-21 18:02:24 -0500

    Removed extra features we don't need to parse from JSON

--- app/src/main/java/com/example/jeffrey/finalprototype/weather/WeatherHttpClient.java
+++ app/src/main/java/com/example/jeffrey/finalprototype/weather/WeatherHttpClient.java
@@ -24,8 +24,6 @@ import java.net.URL;
 public class WeatherHttpClient {
 
 	private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-	private static String IMG_URL = "http://openweathermap.org/img/w/";
-
 	
 	public String getWeatherData(String location) {
 		HttpURLConnection con = null ;
@@ -60,37 +58,5 @@ public class WeatherHttpClient {
 
 		return "";
 				
-	}
-	
-	public byte[] getImage(String code) {
-		HttpURLConnection con = null ;
-		InputStream is = null;
-		try {
-			con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
-			con.setRequestMethod("GET");
-			con.setDoInput(true);
-			con.setDoOutput(true);
-			con.connect();
-			
-			// Let's read the response
-			is = con.getInputStream();
-			byte[] buffer = new byte[1024];
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			
-			while ( is.read(buffer) != -1)
-				baos.write(buffer);
-			
-			return baos.toByteArray();
-	    }
-		catch(Throwable t) {
-			t.printStackTrace();
-		}
-		finally {
-			try { is.close(); } catch(Throwable t) {}
-			try { con.disconnect(); } catch(Throwable t) {}
-		}
-		
-		return null;
-		
 	}
 }
