339324464cdfb17210772812abf8f7577f1ea934
Cory <Coryt0121@gmail.com>
2017-02-21 23:13:20 -0500

    Made the time and number pickers a little nicer and show up in a dialog rather than a new activity

--- app/src/main/java/com/example/jeffrey/finalprototype/PickTime.java
+++ app/src/main/java/com/example/jeffrey/finalprototype/PickTime.java
@@ -1,37 +1,40 @@
 package com.example.jeffrey.finalprototype;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 /**
  * Created by Jeffrey on 2/16/2017.
  */
 
-public class PickTime extends AppCompatActivity {
+public class PickTime extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_time);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
 
-        Button pickTimeButton = (Button) findViewById(R.id.button);
-        final TimePicker tPicker = (TimePicker) findViewById(R.id.timePicker);
-        final Intent returnArrTime = new Intent(this, AddNewCommute.class);
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
 
-
-        pickTimeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                returnArrTime.putExtra("hour", tPicker.getHour());
-                returnArrTime.putExtra("minute", tPicker.getMinute());
-                setResult(Activity.RESULT_OK, returnArrTime);
-                finish();
-            }
-        });
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
+        ((AddNewCommute)getActivity()).setArrivalTime(hourOfDay, minute);
     }
 }
